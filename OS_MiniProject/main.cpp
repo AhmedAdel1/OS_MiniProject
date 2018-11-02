4d067da46f29022c4b73934d9380941a27cac617
Ahmed Adel Abdelhalim <ahmedadel_321@yahoo.com>
2018-11-02 21:58:07 +0200

    Created main & getProcesses fns.
    This program so far reads processes into an array. It reads them from the textfile "output.txt" generated randomly from the generation module.

--- OS_MiniProject/main.cpp
+++ OS_MiniProject/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include "process.h"
+
 
 using namespace std;
 
-int main()
+void getProcesses(Process* arr,unsigned long Size,ifstream& myFile);
+
+int main(int argc, char** argv)
 {
-    cout << "Hello world!" << endl;
+    ifstream myFile;
+    myFile.open(argv[1],ios::in);
+
+    unsigned long Size;
+    myFile >> Size;        /// first line is the number of processes.
+    cout << Size << endl;
+
+    Process* arr = new Process[Size];
+    getProcesses(arr,Size,myFile);
+
+    for(unsigned long i =0; i<Size; i++)
+        cout << arr[i].getProcessID() << "\t" << arr[i].getArrivalTime() << "\t" << arr[i].getBurstTime() << "\t" << arr[i].getPriority() << "\t" << endl;
+
     return 0;
+}
+
+void getProcesses(Process* arr,unsigned long Size,ifstream& myFile)
+{
+    unsigned long prID;
+    double prior,arrivTime,bursTime;
+    for(unsigned long i = 0; i<Size; i++)
+    {
+        myFile >> prID >> arrivTime >> bursTime >> prior;
+
+        arr[i].setProcessID(prID);
+        arr[i].setArrivalTime(arrivTime);
+        arr[i].setBurstTime(bursTime);
+        arr[i].setPriority(prior);
+    }
 }
